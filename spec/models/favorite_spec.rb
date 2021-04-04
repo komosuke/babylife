7b07076583b531c74f20444d7c11ac99f2561f46
komosuke <uw.love0503@gmail.com>
2021-04-04 12:52:18 +0900

    いいね機能のテスト

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -5,5 +5,23 @@ RSpec.describe Favorite, type: :model do
     @favorite = FactoryBot.build(:favorite)
   end
 
-  
+  describe 'いいねの保存' do
+    context 'いいねができるとき' do
+      it 'userとtweetが紐づいていれば保存できる' do
+        expect(@favorite).to be_valid
+      end
+    end
+    context 'いいねができないとき' do
+      it 'userが紐づいていないと投稿できないこと' do
+        @favorite.user = nil
+        @favorite.valid?
+        expect(@favorite.errors.full_messages).to include("Userを入力してください")
+      end
+      it 'tweetが紐づいていないと投稿できないこと' do
+        @favorite.tweet = nil
+        @favorite.valid?
+        expect(@favorite.errors.full_messages).to include("Tweetを入力してください")
+      end
+    end
+  end
 end
